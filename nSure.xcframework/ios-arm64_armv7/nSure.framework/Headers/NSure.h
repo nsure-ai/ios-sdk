3e92ec8dc69dfec0c9acaaa65729499e1f63b8ce
Eli Greenfeld <eli@greenco.tech>
2022-03-03 10:57:28 +0200

    Release 1.2.0
    Signed-off-by: Eli Greenfeld <eli@greenco.tech>

---
+++ nSure.xcframework/ios-arm64_armv7/nSure.framework/Headers/NSure.h
@@ -0,0 +1,36 @@
+//
+//  NSure
+//
+//  Copyright © 2021 nSure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSure : NSObject
+
+/*!
+ Returns (and creates, if needed) a singleton instance of the API.
+ 
+ This method will return a singleton instance of the <code>nSure</code> class for
+ you using the given app ID.
+
+ @param appId        your app ID
+ */
++ (nonnull NSure*)sharedInstanceWithAppID:(NSString *_Nonnull)appId;
+
+/*!
+ *  Returns a previously instantiated singleton instance.
+ */
++ (nullable NSure*)sharedInstance;
+
+/**
+ *  Returns the nSure device ID.
+ */
+@property (nonatomic, strong, readonly) NSString * _Nonnull deviceId;
+
+/*!
+ *  Returns the nSure SDK version.
+ */
+@property (nonatomic, readonly) NSString * _Nonnull version;
+
+@end
